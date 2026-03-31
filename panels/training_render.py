19aac74f39c4cb561495cd0ab6426865e956cda9
bb6 <73919236+bgofish@users.noreply.github.com>
2026-04-01 06:57:30 +1300

    Update training_render.py

--- panels/training_render.py
+++ panels/training_render.py
@@ -1,17 +1,4 @@
-import lfs_api # Hypothetical LFS core API import
-
-class CameraEditorModel:
-    def __init__(self):
-        self.keyframes = [] # List of dicts: {'x': 0, 'y': 0, 'z': 0}
-
-    def load_from_studio(self):
-        # Access the current camera path from the studio core
-        path = lfs_api.get_current_camera_path()
-        self.keyframes = [{"x": p.x, "y": p.y, "z": p.z} for p in path.points]
-
-    def apply_to_studio(self):
-        path = lfs_api.get_current_camera_path()
-        for i, kf in enumerate(self.keyframes):import lfs # This is the actual module provided by LichtFeld Studio
+import lfs # This is the actual module provided by LichtFeld Studio
 
 class CameraEditorModel:
     def __init__(self):
@@ -59,27 +46,3 @@ def setup_data_binding(context):
         idx = int(event.parameters['index'])
         pos = model.keyframes[idx]
         lfs.state.camera.position = lfs.math.Vector3(pos['x'], pos['y'], pos['z'])
-
-            path.points[i].x = float(kf['x'])
-            path.points[i].y = float(kf['y'])
-            path.points[i].z = float(kf['z'])
-        lfs_api.refresh_viewport()
-
-def setup_data_binding(context):
-    model = CameraEditorModel()
-    constructor = context.CreateDataModel("camera_editor")
-    constructor.Bind("keyframes", model.keyframes)
-    
-    # Event Handlers
-    @constructor.OnEvent("refresh_path")
-    def on_refresh(event):
-        model.load_from_studio()
-        
-    @constructor.OnEvent("save_path")
-    def on_save(event):
-        model.apply_to_studio()
-
-    @constructor.OnEvent("goto_keyframe")
-    def on_goto(event):
-        index = event.parameters['index']
-        lfs_api.set_camera_pos(model.keyframes[index])
